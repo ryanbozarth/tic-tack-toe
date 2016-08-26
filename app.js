5e89c6a04ece7e8ee2a39a560b6434161a5b065f
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-08-26 09:33:04 -0700

    finished layout

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
 $(function() {
-    
+  
+
+var nextTurn = 'X'
+// onload present new game
+
+// allow player one to click on square, and give them a X
+
+$(".item" ).click(function() {
+  $(this).append("<i class='material-icons xsymbol'>clear</i>");
+  
+
+});
+
+$(".item" ).dblclick(function() {
+  $(this).append("<i class='material-icons osymbol'>radio_button_unchecked</i>");
+  // prevent them from a second turn
+  
+});
+
+// prevent player one from taking a second turn
+
+function changeTurn(){
+	if(nextTurn == 'X'){
+		nextTurn = '0';
+	}
+	else {
+		nextTurn = 'X';
+	}
+}
+
+// allow player two to double click on a square, and give them 0
+
+// end game after somebody has three in a row
+
+// provide button to start new game 
+
+
+
+
+
     
 
 });
